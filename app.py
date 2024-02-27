11c3a5eee99e42c85a2ce090a3998c1e80f51c88
Justin Rainey <rainey.justin80@gmail.com>
2024-02-26 21:13:30 -0500

    "updates"

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ tabs = ["About", "Projects", "Connect"]
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html', tabs=tabs)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/connect")
+def connect():
+    return render_template("connect.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
