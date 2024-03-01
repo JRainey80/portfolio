f9b4e9bcac3d5d094e0c7b5ef0a3bd23be0002ad
Justin Rainey <rainey.justin80@gmail.com>
2024-03-01 14:28:20 -0500

    smoothed out nav bar

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ tabs = ["About", "Projects", "Connect"]
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html', tabs=tabs)
+    return render_template("index.html", title="Justin Rainey - Portolio", tabs=tabs)
 
 @app.route("/about")
 def about():
