13350606bcd2519fc7a18ecc43a30f0a3c5fa153
Big_Just <rainey.justin80@gmail.com>
2024-03-03 20:28:35 -0500

    a lot. added many files and folders (static, style.css) then added a lot of img's and this thing is starting to look like something!

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
-tabs = ["About", "Projects", "Connect"]
+tabs = ["About", "Projects", "Connect", "Home"]
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html", title="Justin Rainey - Portolio", tabs=tabs)
 
-@app.route("/about")
+@app.route("/about", methods=["GET", "POST"] )
 def about():
     return render_template("about.html")
 
-@app.route("/connect")
+@app.route("/connect", methods=["GET", "POST"])
 def connect():
     return render_template("connect.html")
 
-@app.route("/projects")
+@app.route("/projects", methods=["GET", "POST"])
 def projects():
     return render_template("projects.html")
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    return render_template('index.html')
 
